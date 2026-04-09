dcf3be53163921e857278fc8b846f3b4ca7fc32b
Nguyen Nhu Quoc <nhuquoc1104@gmail.com>
2026-04-09 13:14:04 +0700

    feat(auth): fix AGP deprecation and resource merger crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
+    testImplementation(libs.core.testing)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
