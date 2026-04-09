a803a339f0c5d06018484e25699216fb9d614253
Nguyen Nhu Quoc <nhuquoc1104@gmail.com>
2026-04-09 13:29:15 +0700

    chore(release): del trash

--- app/src/main/java/com/nhom18/flashlock/data/repository/AuthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhom18.flashlock.data.repository;
-
-public interface AuthRepository {
-    void signInWithEmail(String email, String password, AuthResultCallback callback);
-    void registerWithEmail(String email, String password, AuthResultCallback callback);
-    void sendPasswordResetEmail(String email, AuthResultCallback callback);
-    void signInWithGoogleIdToken(String idToken, AuthResultCallback callback);
-}
