5bfa5c5e794cc587e9462f85ad796f810b8bbf1f
Zkhanh2420 <pbrightvc271297@gmail.com>
2026-05-29 15:09:28 +0700

    Update vocabulary topic logic

--- app/src/main/java/com/nhom18/flashlock/data/repository/FirebaseTopicWordRepository.java
+++ app/src/main/java/com/nhom18/flashlock/data/repository/FirebaseTopicWordRepository.java
@@ -1,8 +1,10 @@
 package com.nhom18.flashlock.data.repository;
 
 import com.google.android.gms.tasks.Task;
+import com.nhom18.flashlock.data.model.Topic; // Import ĐÚNG class Topic của dự án
 import com.nhom18.flashlock.data.model.Word;
 import com.nhom18.flashlock.data.remote.FirebaseTopicWordDataSource;
+import com.nhom18.flashlock.data.remote.FirebaseWordDataSource;
 
 import java.util.List;
 
@@ -15,7 +17,9 @@ public class FirebaseTopicWordRepository implements TopicWordRepository {
 
     @Override
     public Task<List<Word>> getTopicWords(String topicId) {
+        if (Topic.MY_WORDS_TOPIC_ID.equals(topicId)) {
+            return new FirebaseWordRepository(new FirebaseWordDataSource()).getAllWords();
+        }
         return dataSource.getTopicWords(topicId);
     }
 }
-
